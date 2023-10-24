5dc40d34bba7c7ebd4d5665bd2302050845ccab5
maxkarlan <maxwell.karlan@gmail.com>
2023-10-23 20:56:22 -0500

    created simple gray-scott diffusion reaction model with random dif rates for A, B, and C. random feed and kill variables as well as random colors for A, B, and C

---
+++ script.js
@@ -0,0 +1,129 @@
+let grid, next;
+// let dA = 01; // Adjusted diffusion rate for A
+// let dB = 0.125; // Adjusted diffusion rate for B
+// let dC = .05;
+// let feed = 0.0367; // Adjusted feed rate
+// let kill = 0.0649; // Adjusted kill rate
+let colorA, colorB, colorC;
+
+let canvasSize = 75; 
+let offsetX, offsetY;
+
+function setup() {
+  dA = random(.7, 1); // Adjusted diffusion rate for A
+  dB = random(0.05, 0.25); // Adjusted diffusion rate for B
+  dC = random(0.05, 0.25);
+  feed = random(0.03, 0.04);
+  kill = random(0.057, 0.065);
+  createCanvas(windowWidth, windowHeight);
+  offsetX = (width - canvasSize) / 2;
+  offsetY = (height - canvasSize) / 2;
+
+  grid = [];
+  next = [];
+  for (let x = 0; x < canvasSize; x++) {
+    grid[x] = [];
+    next[x] = [];
+    for (let y = 0; y < canvasSize; y++) {
+      grid[x][y] = { a: 1, b: 0 };
+      next[x][y] = { a: 1, b: 0 };
+      grid[x][y].c = 0;  // initialize C to 0 everywhere
+      next[x][y].c = 0;
+    }
+
+  }
+  
+  // Adjusted initial seeding for chemical B
+  for (let i = canvasSize * 0.4; i < canvasSize * 0.6; i++) {
+    for (let j = canvasSize * 0.4; j < canvasSize * 0.6; j++) {
+      grid[i][j].b = 1;
+    }
+  }
+  // Assign random colors to chemicals A and B
+  colorA = color(random(255), random(255), random(255));
+  colorB = color(random(255), random(255), random(255));
+  colorC = color(random(255), random(255), random(255));
+
+  frameRate(120);
+}
+
+function draw() {
+  background(220);
+
+  for (let x = 1; x < canvasSize - 1; x++) {
+    for (let y = 1; y < canvasSize - 1; y++) {
+      let a = grid[x][y].a;
+      let b = grid[x][y].b;
+      let c = grid[x][y].c;
+      next[x][y].a = a + (dA * laplaceA(x, y)) - (a * b * b) + (feed * (1 - a));
+      next[x][y].b = b + (dB * laplaceB(x, y)) + (a * b * b) - ((kill + feed) * b);
+      next[x][y].a = constrain(next[x][y].a, 0, 1);
+      next[x][y].b = constrain(next[x][y].b, 0, 1);
+      next[x][y].c = c + (dC * laplaceC(x, y)) - (a * b * c) + (feed * (1 - c));
+      next[x][y].c = constrain(next[x][y].c, 0, 1);
+    }
+  }
+
+  for (let x = 0; x < canvasSize; x++) {
+    for (let y = 0; y < canvasSize; y++) {
+      let a = next[x][y].a;
+      let b = next[x][y].b;
+      let c = next[x][y].c;
+      let col = lerpColor(lerpColor(colorA, colorB, b), colorC, c);
+      set(x + offsetX, y + offsetY, col);
+    }
+  }
+
+  updatePixels();
+  swap();
+}
+
+
+function laplaceA(x, y) {
+  let sumA = 0;
+  sumA += grid[x][y].a * -1;
+  sumA += grid[x-1][y].a * 0.2;
+  sumA += grid[x+1][y].a * 0.2;
+  sumA += grid[x][y-1].a * 0.2;
+  sumA += grid[x][y+1].a * 0.2;
+  sumA += grid[x-1][y-1].a * 0.05;
+  sumA += grid[x+1][y-1].a * 0.05;
+  sumA += grid[x+1][y+1].a * 0.05;
+  sumA += grid[x-1][y+1].a * 0.05;
+  return sumA;
+}
+
+function laplaceB(x, y) {
+  let sumB = 0;
+  sumB += grid[x][y].b * -1;
+  sumB += grid[x-1][y].b * 0.2;
+  sumB += grid[x+1][y].b * 0.2;
+  sumB += grid[x][y-1].b * 0.2;
+  sumB += grid[x][y+1].b * 0.2;
+  sumB += grid[x-1][y-1].b * 0.05;
+  sumB += grid[x+1][y-1].b * 0.05;
+  sumB += grid[x+1][y+1].b * 0.05;
+  sumB += grid[x-1][y+1].b * 0.05;
+  return sumB;
+}
+
+function laplaceC(x, y) {
+    let sumC = 0;
+    // Define the Laplacian for C, similar to A and B
+    sumC += grid[x][y].c * -1;
+    sumC += grid[x-1][y].c * 0.2;
+    sumC += grid[x+1][y].c * 0.2;
+    sumC += grid[x][y-1].c * 0.2;
+    sumC += grid[x][y+1].c * 0.2;
+    sumC += grid[x-1][y-1].c * 0.05;
+    sumC += grid[x+1][y-1].c * 0.05;
+    sumC += grid[x+1][y+1].c * 0.05;
+    sumC += grid[x-1][y+1].c * 0.05;
+    return sumC;
+}
+
+function swap() {
+  let temp = grid;
+  grid = next;
+  next = temp;
+}
